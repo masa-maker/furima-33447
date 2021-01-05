4788d59760787d7608ea27c2ff4dea774ed478c0
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-05 18:02:07 +0900

    ログイン、ログアウト機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, 
          :recoverable, :rememberable, :validatable
-
-
-        end
+  validates :nick_name, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message:' Full-width characters'} 
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message:' Full-width characters'} 
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: ' Full-width katakana characters'} 
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: ' Full-width katakana characters'} 
+  validates :birth_id, presence: true
+  validates :password, format: {with: (/[a-z\d]{6,}/i), message: 'both letters and numbers'}
+end
