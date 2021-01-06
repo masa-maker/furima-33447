91e3d5e892fcbd892d8fffb930c00e98f5d1d800
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-06 18:26:35 +0900

    出品保存機能　実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,9 +5,17 @@ before_action :move_to_index, only: :new
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,21 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :info
+    validates :price, numericality: { only_integer:true, greater_than: 300, less_then: 9999999, message: "Half-width number" }
+  end
+
+    with_options presence: true, numericality:  {other_than: 1, message: "Select"} do
+      validates :category_id
+      validates :item_status_id
+      validates :shipping_charge_id
+      validates :prefecture_id
+      validates :shipping_date_id
+    end
+
   belongs_to :category
   belongs_to :item_status
   belongs_to :prefecture
@@ -7,18 +23,4 @@ class Item < ApplicationRecord
   belongs_to :shipping_date
   belongs_to :user
   has_one_attached :image
-
-  with_options presence: true do
-    validates :image
-    validates :item_name
-    validates :info
-    validates :price, numericality: { only_integer:true, greater_than: 300, less_then: 9999999 }
-    with_options numericality:  {other_than: 1 } do
-      validates :category_id
-      validates :item_status_id
-      validates :shipping_charge_id
-      validates :prefecture_id
-      validates :shipping_date_id
-    end
-  end
 end
