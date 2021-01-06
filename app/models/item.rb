d0c4cce3e2bc6cb54c287c8b5634bcfff87db10f
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-06 15:54:14 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,17 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :info
+    validates :price, numericality: { only_integer:true, greater_than: 300, less_then: 9999999 }
+    with_options numericality:  {other_than: 1 } do
+      validates :category_id
+      validates :item_status_id
+      validates :shipping_charge_id
+      validates :prefecture_id
+      validates :shipping_date_id
+    end
+  end
 end
