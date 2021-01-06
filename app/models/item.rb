230612cac0521f19786d81402589ff4a6a2e9bc8
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-06 14:53:05 +0900

    Active_hashモデルアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :prefecture
+  belongs_to :shipping_charge
+  belongs_to :shipping_date
   belongs_to :user
   has_one_attached :image
+
 end
