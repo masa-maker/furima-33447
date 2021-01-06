27c7285bf978154df8a5bff7ec90bdee28bafe08
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-06 14:12:25 +0900

    itemモデル作製

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,21 @@
 class ItemsController < ApplicationController
+before_action :move_to_index, only: :new
+
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def move_to_index 
+    unless user_signed_in?
+      redirect_to user_session_path
+    end
+  end
+
 end
