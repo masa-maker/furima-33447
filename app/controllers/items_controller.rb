3ff4574ed4a2562866d9bfe30cdc31787d04a483
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-06 14:24:24 +0900

    itemストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,11 +11,13 @@ before_action :move_to_index, only: :new
   end
 
   private
+  def item_params
+    params.require(:item).permit(:item_name, :info, :price, :category_id, :item_status_id, :shipping_charge_id, :prefecture_id, :shipping_date_id, :image).merge(user_id: current_user.id)
+  end
 
   def move_to_index 
     unless user_signed_in?
       redirect_to user_session_path
     end
   end
-
 end
