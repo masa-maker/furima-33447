d0cc0e6d382e646f8296fab8c047a52b9466742e
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-06 11:34:16 +0900

    指摘箇所　修正３

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nick_name, presence: true
   with_options presence: true do
+    validates :nick_name
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: ' Full-width characters' }
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: ' Full-width characters' }
     validates :last_name_kana, format: { with: /\A[ァ-ヶ一]+\z/, message: "Full-width katakana characters" }
