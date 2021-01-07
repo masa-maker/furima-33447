48e726d4a3167a77fd32afa1b29b39a576e5484e
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-07 17:59:02 +0900

    rubocop 修正済み

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,22 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  
+
   with_options presence: true do
     validates :image
     validates :item_name
     validates :info
-    validates :price, numericality: { only_integer:true, greater_than: 300, less_than: 9999999, message: "Out of setting range" }
-    validates :price, numericality: { with: /\A[0-9]+\z/, message: "Half-width number"}
+    validates :price,
+              numericality: { only_integer: true, greater_than: 300, less_than: 9_999_999, message: 'Out of setting range' }
+    validates :price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
   end
 
-    with_options presence: true, numericality:  {other_than: 1, message: "Select"} do
-      validates :category_id
-      validates :item_status_id
-      validates :shipping_charge_id
-      validates :prefecture_id
-      validates :shipping_date_id
-    end
+  with_options presence: true, numericality: { other_than: 1, message: 'Select' } do
+    validates :category_id
+    validates :item_status_id
+    validates :shipping_charge_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
 
   belongs_to :category
   belongs_to :item_status
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,12 @@ class User < ApplicationRecord
       validates :first_name
       validates :last_name
     end
-    with_options format: { with: /\A[ァ-ヶ一]+\z/, message: "Full-width katakana characters" } do
+    with_options format: { with: /\A[ァ-ヶ一]+\z/, message: 'Full-width katakana characters' } do
       validates :last_name_kana
       validates :first_name_kana
     end
     validates :birth_id
   end
-    validates :password, format: { with: (/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i), message: 'both letters and numbers' }
-    has_many :items
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'both letters and numbers' }
+  has_many :items
 end
