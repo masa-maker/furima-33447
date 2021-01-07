c2059e6c9a0f2253c7a21d88338d83b02371c015
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-07 19:34:44 +0900

    ビュー遷移　制限機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: :edit
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -24,6 +25,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
@@ -34,5 +36,11 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :info, :price, :category_id, :item_status_id, :shipping_charge_id, :prefecture_id,
                                  :shipping_date_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    if user_signed_in? != current_user.id
+      redirect_to action: :index
+    end
   end
 end
