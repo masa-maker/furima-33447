66b2502740ddcbd23f9500c53ba1bd01704aee69
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-09 18:18:03 +0900

    購入画面遷移

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,15 +1,17 @@
 class PurchasesController < ApplicationController
 
-  def index
-    @item = Item.find[params[:id]]
+  def index   
+    @item_purchase = ItemPurchase.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    
   end
 
   private
 
   def purchase_params
-    params.require(:item).permit(:image, :item_name, :shipping_charge_id, :price).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:item_purchase).permit(:token, :portal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchase)
   end
 end
--- app/models/item_purchase.rb
+++ app/models/item_purchase.rb
@@ -1,18 +1,16 @@
 class ItemPurchase 
   include ActiveModel::Model
   attr_accessor :token, :portal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchase
-
-  
-      with_options presence: true do
+    with_options presence: true do
         validates :token
-        validates :portal_code, format { with: /\A\d{3}[-]\d{4}\z/, message: "Input correctly" }
+        validates :portal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "Input correctly" }
         validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
         validates :city
         validates :address
         validates :phone_number, numericality: { greater_than: 1, less_than: 11, message: "Input only number"}
-      end
+    end
   def save
-    purchase = Purchase.create(:user_id, :item_id, :created_at) #購入者の情報を保存し、parchase変数にいれる
+    purchase = Purchase.create(user_id: current_user.id, item_id: params[:item_id]) #購入者の情報を保存し、parchase変数にいれる
     Address.create(:portal_code, :prefecture_id, :city, :address, :building_name, :phone_number, purchase_id: purchase.id)
   end
 end
