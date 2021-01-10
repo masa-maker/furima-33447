6858cc6188b03482c029c16dfcdc330b5d3500b1
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-10 14:48:22 +0900

    クレジット決済機能実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,12 +6,26 @@ class PurchasesController < ApplicationController
   end
 
   def create
-    
+    @item_purchase = ItemPurchase.new(purchase_params)
+    @item = Item.find(params[:item_id]) 
+    if @item_purchase.valid?
+      Payjp.api_key = "sk_test_3272fea7daacc1d87f1325dc"
+      Payjp::Charge.create(
+        amount: @item.price, # 商品の値段
+        card: purchase_params[:token],    # カードトークン
+        currency: 'jpy'
+      ) 
+      @item_purchase.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
   end
 
   private
 
   def purchase_params
-    params.require(:item_purchase).permit(:token, :portal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchase)
+    params.require(:item_purchase).permit(:portal_code, 
+      :prefecture_id, :city, :address, :building_name, :phone_number, :purchase_id, :item_id).merge(user_id: current_user.id, token: params[:token])
   end
 end
